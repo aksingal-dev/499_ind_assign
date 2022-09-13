f287a2274af4dea81230bc322a54e16c906bc322
akshatsingal00@gmail.com <akshatsingal00@gmail.com>
2022-09-12 18:22:12 -0700

    added main

---
+++ Sort.java
@@ -0,0 +1,9 @@
+public class Sort {
+    
+    public static void main(String[] args) {
+        String[] t1 = {"1", "3", "5", "7", "8"};
+        String[] t2 = {"55", "-5", "67", "0", "100"};
+        String[] t3 = {"-2", "-1", "0", "1","2"};
+    
+    }
+}
