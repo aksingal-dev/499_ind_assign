f139ac86dbc9974b9f8287370ab95846e36c335b
akshatsingal00@gmail.com <akshatsingal00@gmail.com>
2022-09-12 18:29:15 -0700

    Modified main to test ascending

--- Sort.java
+++ Sort.java
@@ -1,9 +1,18 @@
+import java.util.Arrays;
+
 public class Sort {
     
     public static void main(String[] args) {
         String[] t1 = {"1", "3", "5", "7", "8"};
         String[] t2 = {"55", "-5", "67", "0", "100"};
         String[] t3 = {"-2", "-1", "0", "1","2"};
-    
+       
+        asc ascend = new asc();
+        System.out.println("\n ################################################# Test 2 ################################################### \n");
+        ascend.ascend(t1);
+        System.out.println("\n ################################################# Test 2 ################################################### \n");
+        ascend.ascend(t2);
+        System.out.println("\n ################################################# Test 2 ################################################### \n");
+        ascend.ascend(t3);
     }
 }
