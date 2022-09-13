4e379f062e74bad465c14ffc58d229dc22555972
akshatsingal00@gmail.com <akshatsingal00@gmail.com>
2022-09-12 18:50:33 -0700

    Completed Main.java

--- Sort.java
+++ Sort.java
@@ -6,13 +6,63 @@ public class Sort {
         String[] t1 = {"1", "3", "5", "7", "8"};
         String[] t2 = {"55", "-5", "67", "0", "100"};
         String[] t3 = {"-2", "-1", "0", "1","2"};
-       
+
+        Integer[] a1 = {1, 3, 5, 7, 8};
+        Integer[] a2 = {-5, 0, 55, 67, 100};
+        Integer[] a3 = {-2, -1, 0, 1, 2};
+        
+        Integer[] d1 = {8, 7, 5, 3, 1};
+        Integer[] d2 = {100, 67, 55, 0, -5};
+        Integer[] d3 = {2, 1, 0, -1, -2};
+
+        Integer[] tester = new Integer[5];
+        
+        //TESTING ASCENDING SORT
         asc ascend = new asc();
         System.out.println("\n ################################################# Test 2 ################################################### \n");
-        ascend.ascend(t1);
+        tester = ascend.ascend(t1);
+        if(Arrays.equals(tester, a1)){
+            System.out.println("\n Test Passed\n");
+        }else{
+            System.out.println(" \n Test Failed \n");
+        }
         System.out.println("\n ################################################# Test 2 ################################################### \n");
-        ascend.ascend(t2);
+        tester = ascend.ascend(t2);
+        if(Arrays.equals(tester, a2)){
+            System.out.println("\n Test Passed\n");
+        }else{
+            System.out.println(" \n Test Failed \n");
+        }
         System.out.println("\n ################################################# Test 2 ################################################### \n");
-        ascend.ascend(t3);
+        tester = ascend.ascend(t3);
+        if(Arrays.equals(tester, a3)){
+            System.out.println("\n Test Passed\n");
+        }else{
+            System.out.println(" \n Test Failed \n");
+        }
+
+         //TESTING DESCENDING SORT
+         dsc descend = new dsc();
+         System.out.println("\n ################################################# Test 2 ################################################### \n");
+         tester = descend.ascend(t1);
+         if(Arrays.equals(tester, d1)){
+             System.out.println("\n Test Passed\n");
+         }else{
+             System.out.println(" \n Test Failed \n");
+         }
+         System.out.println("\n ################################################# Test 2 ################################################### \n");
+         tester = descend.ascend(t2);
+         if(Arrays.equals(tester, d2)){
+             System.out.println("\n Test Passed\n");
+         }else{
+             System.out.println(" \n Test Failed \n");
+         }
+         System.out.println("\n ################################################# Test 2 ################################################### \n");
+         tester = descend.ascend(t3);
+         if(Arrays.equals(tester, d3)){
+             System.out.println("\n Test Passed\n");
+         }else{
+             System.out.println(" \n Test Failed \n");
+         }
     }
 }
